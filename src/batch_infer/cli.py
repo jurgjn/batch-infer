27bf1a56f2594f4276367d4a2f9d3bbbefaaff18
jurgjn <jurgjn@users.noreply.github.com>
2026-03-17 10:56:31 +0100

    Move batch-infer script to python & add sanity checks (cont.)

--- src/batch_infer/cli.py
+++ src/batch_infer/cli.py
@@ -1,39 +1,68 @@
-#!/usr/bin/env bash
-BATCH_INFER_DIR=$(dirname $([ -L $0 ] && readlink -f $0 || echo $0))
-BATCH_INFER_TARGET=$1
-RESULTS_PREFIX=$(realpath "$2")
-RESULTS_BASENAME=$(basename "$RESULTS_PREFIX")
-XTRA_ARGS="$3"
-LOGS_DATE=$RESULTS_PREFIX/.snakemake-eu/logs/`date +"%y-%m-%d"`
-JOBNAME="batch-infer:$BATCH_INFER_TARGET=$RESULTS_BASENAME"
-# Use prefix-specific config file if one exists
-if [ -f "$RESULTS_PREFIX/config.yaml" ]; then
-    CONFIGFILE_LOCAL="$RESULTS_PREFIX/config.yaml"
-else
-    CONFIGFILE_LOCAL=""
-fi
-cat - <<EOF
-#!/usr/bin/env bash
-#SBATCH --job-name=$JOBNAME
+
+import collections, contextlib, copy, filecmp, glob, io, itertools, json, os, os.path, subprocess, sys, time, zipfile, warnings
+from datetime import datetime
+from pathlib import Path
+from pprint import pprint
+
+import click
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+@click.group()
+def cli():
+    pass
+
+@cli.command(short_help='Check for files & output sbatch script to start a run')
+@click.argument('target', type=str)
+@click.argument('results_path', type=click.Path(exists=True, file_okay=False, dir_okay=True, writable=True, path_type=Path))
+@click.option('--dry-run', is_flag=True, default=False)
+def start(target, results_path, dry_run):
+    """
+    Example:
+        batch_infer sbatch alphafold3_datafill_missing projects/ubi_ncORFs/pairwise --dry-run | sbatch
+    """
+
+    jobname = f'batch_infer:{target}'
+    output_path = results_path / f'.snakemake-eu/logs/{datetime.today().strftime("%y-%m-%d")}/{jobname}-%j.txt'
+
+    activate_path = Path(__file__).parent.parent.parent.resolve() / '.venv/bin/activate'
+    snakefile_path = Path(__file__).parent.parent.parent.resolve() / f'workflow/targets/{target}.smk'
+    configfile_path1 = Path(__file__).parent.parent.parent.resolve() / 'workflow/config/defaults.yaml'
+    configfile_path2 = results_path / f'config.yaml'
+    for path_ in [activate_path, snakefile_path, configfile_path1, configfile_path2,]:
+        if not path_.is_file():
+            eprint(f'Not a file: {path_}')
+            sys.exit(1)
+
+    profile_path = Path(__file__).parent.parent.parent.resolve() / 'workflow/profiles/default'
+    for path_ in [results_path, profile_path,]:
+        if not path_.is_dir():
+            eprint(f'Not a directory: {path_}')
+            sys.exit(1)
+
+    xtra_args = ''
+    if dry_run:
+        xtra_args = '--dry-run'
+
+    print(f"""#!/usr/bin/env bash
+#SBATCH --job-name={jobname}
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4G
 #SBATCH --tmp=16G
 #SBATCH --time=7-00:00:00
-#SBATCH --output=$LOGS_DATE/$JOBNAME-%j.txt
-BATCH_INFER_DIR="$BATCH_INFER_DIR"
-BATCH_INFER_TARGET="$BATCH_INFER_TARGET"
-RESULTS_PREFIX="$RESULTS_PREFIX"
-XTRA_ARGS="$XTRA_ARGS"
+#SBATCH --output={output_path.resolve()}
 module load stack/2025-06 python/3.13.0 eth_proxy
-if [ ! -f $BATCH_INFER_DIR/.venv/bin/activate ]; then
-    source $BATCH_INFER_DIR/software/venv/create
-fi
-source $BATCH_INFER_DIR/.venv/bin/activate
-snakemake $BATCH_INFER_TARGET $XTRA_ARGS \\
-    --snakefile $BATCH_INFER_DIR/workflow/targets/$BATCH_INFER_TARGET.smk \\
-    --configfile $BATCH_INFER_DIR/workflow/config/defaults.yaml $CONFIGFILE_LOCAL \\
-    --profile=$BATCH_INFER_DIR/workflow/profiles/default \\
-    --directory $RESULTS_PREFIX \\
+source {activate_path.resolve()}
+snakemake {target} {xtra_args} \\
+    --snakefile {snakefile_path.resolve()} \\
+    --configfile {configfile_path1.resolve()} {configfile_path2.resolve()} \\
+    --profile={profile_path.resolve()} \\
+    --directory {results_path.resolve()} \\
     --rerun-triggers mtime
-myjobs -j \$SLURM_JOB_ID
-EOF
+myjobs -j $SLURM_JOB_ID
+""")
+
+@cli.command(short_help='Status')
+def status():
+    subprocess.run('squeue --format="%.18i %.12P %.128j %.8T %.16M %.16l %40R" | column -t --table-right 1,5,6', shell=True) 
